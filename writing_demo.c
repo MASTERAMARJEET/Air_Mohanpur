6fdcb58be6189730dc84338f642f6773f92c9f54
CrappyBro007 <CrappyBro007@github.com>
2020-11-12 20:19:21 +0530

    Initialization Changes in for loop
    Was showing error in DevC++

--- writing_demo.c
+++ writing_demo.c
@@ -9,18 +9,23 @@ int main(){
 
     // defining empty array where we will read and save the data
     Admin buffer[10];
+    
+    //defining empty flight array 
+	Flight flightname[10];
 
     printf("The size of the struct is: %lu\n",sizeof(Admin));
 
     // Saving the admins data to file (same data 10 times for example sake)
-    for (int j=0;j<=9;j++){
+    int j;
+    for (j=0;j<=9;j++){
         appendFile("Write.txt",&admins,sizeof(Admin),2);
     }
 
     // Reading it back from file into buffer.
     int num = readFile("Write.txt",&buffer,sizeof(Admin),4);
     printf("Total number of admins read from the file: %d\n",num);
-    for (int i=0;i<=num-1;++i){
+    int i;
+    for (i=0;i<=num-1;++i){
         printf("%d. Id: %s Password: %s\n",i+1,buffer[i].id,buffer[i].password);
     }
 
